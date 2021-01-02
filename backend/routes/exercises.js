55896c2cd77e588ba6d17e375adcc13ca77b8a4f
Stephan <tape.stephan@gmail.com>
2021-01-02 23:00:10 +0100

    Fix routing

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -1,28 +1,28 @@
-const router = require('express').Router();
 let Exercise = require('../models/exercise.model');
 
-router.route('/').get((req, res) => {
-    Exercise.find()
-        .then(exercises => res.json(exercises))
-        .catch(err => res.status(400).json('Error: ' + err));
-});
+module.exports = function (app) {
 
-router.route('/add').post((req, res) => {
-    const username = req.body.username;
-    const description = req.body.description;
-    const duration = Number(req.body.duration);
-    const date = Date.parse(req.body.date);
-
-    const newExercise = new Exercise({
-        username,
-        description,
-        duration,
-        date,
+    app.route('/exercises').get((req, res) => {
+        Exercise.find()
+            .then(exercises => res.json(exercises))
+            .catch(err => res.status(400).json('Error: ' + err));
     });
 
-    newExercise.save()
-        .then(() => res.json('Exercise added!'))
-        .catch(err => res.status(400).json('Error: ' + err));
-});
+    app.route('/exercises/add').post((req, res) => {
+        const username = req.body.username;
+        const description = req.body.description;
+        const duration = Number(req.body.duration);
+        const date = Date.parse(req.body.date);
 
-module.exports = router;
+        const newExercise = new Exercise({
+            username,
+            description,
+            duration,
+            date,
+        });
+
+        newExercise.save()
+            .then(() => res.json('Exercise added!'))
+            .catch(err => res.status(400).json('Error: ' + err));
+    });
+}
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,20 +1,20 @@
-const router = require('express').Router();
 let User = require('../models/user.model');
 
-router.route('/').get((req, res) => {
-    User.find()
-        .then(users => res.json(users))
-        .catch(err => res.status(400).json('Error: ' + err));
-});
+module.exports = function (app) {
 
-router.route('/add').post((req, res) => {
-    const username = req.body.username;
+    app.route('/users').get((req, res) => {
+        User.find()
+            .then(users => res.json(users))
+            .catch(err => res.status(400).json('Error: ' + err));
+    });
 
-    const newUser = new User({username});
+    app.route('/users/add').post((req, res) => {
+        const username = req.body.username;
 
-    newUser.save()
-        .then(() => res.json('User added!'))
-        .catch(err => res.status(400).json('Error: ' + err));
-});
+        const newUser = new User({username});
 
-module.exports = router;
+        newUser.save()
+            .then(() => res.json('User added!'))
+            .catch(err => res.status(400).json('Error: ' + err));
+    });
+}
